9bca67cf9154321c92f6b569edddfee9ced82e49
jonah18 <jonahmg@umich.edu>
2017-06-14 11:07:09 +0300

    test db

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Yo, it's working!"
+	host = 'search-houseshark-ixotnqxkzathhe3en6ql2egzha.us-east-1.es.amazonaws.com'
+    awsauth = AWS4Auth('AKIAI7S7SHF547UFU5MA', '1tm6QTj6EgNy5lx//F3IHwmkSoaVBtzEuaqh+Yu6', 'us-east-1', 'es')
+
+    es = Elasticsearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+
+    res = es.get(index="zigbee-index", doc_type='zigbee')
+	print(res['_source'])
+
+
+    return res
 
 if __name__ == "__main__":
     app.run()
