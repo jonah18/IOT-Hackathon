09c1afefb77b18b212cb2dd689edc020030eb4db
jonah18 <jonahmg@umich.edu>
2017-06-14 11:18:50 +0300

    test db

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	host = 'search-houseshark-ixotnqxkzathhe3en6ql2egzha.us-east-1.es.amazonaws.com'
-    awsauth = AWS4Auth('AKIAI7S7SHF547UFU5MA', '1tm6QTj6EgNy5lx//F3IHwmkSoaVBtzEuaqh+Yu6', 'us-east-1', 'es')
+	awsauth = AWS4Auth('AKIAI7S7SHF547UFU5MA', '1tm6QTj6EgNy5lx//F3IHwmkSoaVBtzEuaqh+Yu6', 'us-east-1', 'es')
 
-    es = Elasticsearch(
-        hosts=[{'host': host, 'port': 443}],
-        http_auth=awsauth,
-        use_ssl=True,
-        verify_certs=True,
-        connection_class=RequestsHttpConnection
-    )
+	es = Elasticsearch(
+		hosts=[{'host': host, 'port': 443}],
+		http_auth=awsauth,
+		use_ssl=True,
+		verify_certs=True,
+		connection_class=RequestsHttpConnection
+	)
 
 	res = es.search(index="zigbee-index", doc_type="zigbee", body={"query": {"match_all": {}}})
-	print("%d documents found" % res['hits']['total'])
+	print "%d documents found" % res['hits']['total']
 
 	for doc in res['hits']['hits']:
 		print("%s) %s" % (doc['_id'], doc['_source']))
 
-    return "Hi"
+	return "Hi"
 
 if __name__ == "__main__":
     app.run()
